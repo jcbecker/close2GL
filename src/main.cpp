5fe53cb78eb74d39480bcdfaa59a4e7e2b2bfb3c
jcbecker <beckerjc95@gmail.com>
2019-04-12 11:51:16 -0300

    Removing Awesome container from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,6 @@ int main(){
 
     bool drawCubeFlag = true;
     bool drawCowGiseleFlag = true;
-    bool drawAwesomeContainers = true;
     ImVec4 objImColors = ImVec4(0.7f, 0.7f, 0.7f, 1.0f); //Clear color
 
 
@@ -147,155 +146,6 @@ int main(){
     if (no_background)      window_flags |= ImGuiWindowFlags_NoBackground;
     if (no_bring_to_front)  window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
     if (no_close)           p_open = NULL;
-    
-    float vertices[] = {
-        // positions       // colors          // texture coords
-        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // top left      2
-         0.5f, -0.5f,  0.5f, 0.0f, 1.0f, 0.0f,  1.0f, 0.0f, // bottom right  1
-         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f,  // top right    3
-
-         0.5f,  0.5f,  0.5f, 0.0f, 0.0f, 1.0f,  1.0f, 1.0f, // top left      2
-        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,  0.0f, 1.0f, // bottom left   0
-        -0.5f, -0.5f,  0.5f, 0.0f, 1.0f, 0.0f,  0.0f, 0.0f, // bottom right  1
-
-
-         0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  1.0f, 1.0f, // bottom left   4
-         0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  1.0f, 0.0f, // bottom right  5
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // top left      6
-        
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // top left      6
-        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  0.0f, 1.0f, // bottom right  5
-         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f, // top right     7
-
-
-         0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,  0.0f, 1.0f, // bottom left   0
-         0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  0.0f, 0.0f, // bottom left   4
-         0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 1.0f,  1.0f, 0.0f, // top left      2
-
-         0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 1.0f,  1.0f, 0.0f, // top left      2
-         0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,  1.0f, 1.0f, // bottom left   4
-         0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f,  0.0f, 1.0f, // top left      6
-
-
-        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  1.0f, 0.0f, // bottom right  1
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  0.0f, 0.0f, // bottom right  5
-        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,  0.0f, 1.0f, // top right     3
-
-        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,  0.0f, 1.0f, // top right     3
-        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 0.0f,  1.0f, 1.0f, // bottom right  5
-        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 0.0f, // top right     7
-
-
-        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,  0.0f, 1.0f, // bottom left   0
-         0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 0.0f,  1.0f, 1.0f, // bottom right  1
-         0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  1.0f, 0.0f, // bottom left   4
-
-         0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  1.0f, 0.0f, // bottom left   4
-        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  0.0f, 0.0f, // bottom right  1
-        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 0.0f,  0.0f, 1.0f, // bottom right  5
-
-
-         0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,  1.0f, 0.0f, // top left      2
-         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f, // top right     3
-        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f,  0.0f, 1.0f, // top left      6
-
-        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f,  0.0f, 1.0f, // top left      6
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,  0.0f, 0.0f, // top right     3
-         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 0.0f  // top right     7
-    };
-
-    //Cubes positions
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  8.0f,  0.0f), 
-        glm::vec3( 2.0f,  5.0f, -15.0f), 
-        glm::vec3(-1.5f, -2.2f, -2.5f),  
-        glm::vec3(-3.8f, -2.0f, -12.3f),  
-        glm::vec3( 2.4f, -0.4f, -3.5f),  
-        glm::vec3(-1.7f,  3.0f, -7.5f),  
-        glm::vec3( 1.3f, -2.0f, -2.5f),  
-        glm::vec3( 1.5f,  2.0f, -2.5f), 
-        glm::vec3( 1.5f,  0.2f, -1.5f), 
-        glm::vec3(-1.3f,  1.0f, -1.5f)  
-    };
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO); 
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // Texure coords
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(0);
-
-    // load and create a texture 
-    // -------------------------
-    // texture 1
-    // ---------
-    unsigned int texture1, texture2;
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true); 
-    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load("../assets/textures/container.jpg", &width, &height, &nrChannels, 0);
-    if (data){
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else{
-        std::cout << "Failed to load texture1" << std::endl;
-    }
-    stbi_image_free(data);
-
-    // texture 2
-    // ---------
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    data = stbi_load("../assets/textures/awesomeface.png", &width, &height, &nrChannels, 0);
-    if (data){
-        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else{
-        std::cout << "Failed to load texture2" << std::endl;
-    }
-    stbi_image_free(data);
-
-    shaderObj.use(); // don't forget to activate/use the shader before setting uniforms!
-    shaderObj.setInt("texture1", 0);
-    shaderObj.setInt("texture2", 1);
-
 
     // Imgui variables
     ImVec4 clear_color = ImVec4(0.2f, 0.2f, 0.2f, 1.00f); //Clear color
@@ -331,15 +181,6 @@ int main(){
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // bind textures on corresponding texture units
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-        
-        // Active shader
-        shaderObj.use();
-
         // create transformations
         glm::mat4 model; // make sure to initialize matrix to identity matrix first
         glm::mat4 view          = glm::mat4(1.0f);
@@ -350,37 +191,15 @@ int main(){
 
         view = camera.GetViewMatrix();
 
-        // get matrix's uniform location and set matrix
-        shaderObj.setMat4("view", view);
-        shaderObj.setMat4("projection", projection);
-
-
         glPolygonMode(GL_FRONT_AND_BACK, drawPrimitive);
+        
+        
         //  Draw Cals
-
-        if(drawAwesomeContainers){
-            glBindVertexArray(VAO);
-            // glDrawElements(GL_TRIANGLES, 6 * 6, GL_UNSIGNED_INT, 0);
-            for(unsigned int i = 0; i < 10; i++){
-                model = glm::mat4(1.0f);
-                model = glm::translate(model, cubePositions[i]);
-                float angle = 20.0f * (i +1); 
-                // model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-                model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-                shaderObj.setMat4("model", model);
-                glDrawArrays(GL_TRIANGLES, 0, 6 * 6);
-            }
-            glBindVertexArray(0);
-        }
-
-
         loadObjectShader.use();
         loadObjectShader.setMat4("view", view);
         loadObjectShader.setMat4("projection", projection);
         glm::vec3 colorObejects(objImColors.x, objImColors.y, objImColors.z);//   objImColors
         loadObjectShader.setVec3("uColor", colorObejects);
-        
-        
         float scalasetNorm;
 
         if(drawCubeFlag){
@@ -425,9 +244,6 @@ int main(){
                 ImGui::Checkbox("DrawCube", &drawCubeFlag);
                 ImGui::Text("Loaded Cow:");
                 ImGui::Checkbox("Draw Cow", &drawCowGiseleFlag);
-
-                ImGui::Text("Inbuild Awesome Containers:");
-                ImGui::Checkbox("Draw Awesome Containers", &drawAwesomeContainers);
 
                 ImGui::Text("Objects color:");
                 ImGui::Indent();{
@@ -574,9 +390,7 @@ int main(){
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    
     glDeleteVertexArrays(1, &cubeojb.VAO);
     glDeleteBuffers(1, &cubeojb.VBO);
     
